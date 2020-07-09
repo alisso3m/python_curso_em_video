86831d485854a91d32fcdf084a3d1f55dac48c91
alisso3m <52075714+alisso3m@users.noreply.github.com>
2020-07-09 16:47:58 -0300

    Add files via upload

---
+++ Aula06b.py
@@ -0,0 +1,6 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite o segundo valor '))
+s = n1 + n2
+d = int(s / 2)
+print('A soma entre {} e {} vale {} a divisão por 2 será {}'.format(n1, n2, s, d))
+print('Valor de N1 {}'.format(n1))
